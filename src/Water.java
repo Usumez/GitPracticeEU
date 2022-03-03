da4e9ca18dd650705f9e470e8a0ca9af6bc7feb3
Usumez <usumez74@gmail.com>
2022-03-03 18:35:06 +0000

    First Day

---
+++ src/Water.java
@@ -0,0 +1,8 @@
+public class Water {
+
+    public static void main(String[] args) {
+        System.out.println("First day at work");
+        System.out.println("Did UI Automation, did verification of DB and");
+
+    }
+}
